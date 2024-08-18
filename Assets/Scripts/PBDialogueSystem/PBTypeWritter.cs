4ea63d5f1138092e76a96c2dabcae28e66153587
蒋沛和 <jphandll@gmail.com>
2024-08-18 15:02:55 +0800

    音效

--- Assets/Scripts/PBDialogueSystem/PBTypeWritter.cs
+++ Assets/Scripts/PBDialogueSystem/PBTypeWritter.cs
@@ -8,15 +8,16 @@ using UnityEngine;
 public class PBTypeWritter : MonoBehaviour
 {
     public TextMeshProUGUI textMeshPro;
-    public float typingSpeed = 0.1f; // 每个字符的时间间隔
+    public float           typingSpeed = 0.1f; // 每个字符的时间间隔
 
-    private int _currentCharIndex;
+    private int   _currentCharIndex;
     private Tween _typewriterTween;
-    
+
     public Conversation conversation;
 
-    public string fullText = "";
+    public AudioSource audioSource;
 
+    public string fullText = "";
 
 
     public void ShowFullText()
@@ -38,6 +39,7 @@ public class PBTypeWritter : MonoBehaviour
         {
             textMeshPro = GetComponent<TextMeshProUGUI>();
         }
+
         textMeshPro.text = "";
         _currentCharIndex = 0;
 
@@ -47,16 +49,28 @@ public class PBTypeWritter : MonoBehaviour
 
     void StartTypewriterEffect()
     {
-        _typewriterTween = DOTween.To(() => _currentCharIndex, x => _currentCharIndex = x, fullText.Length,
-                fullText.Length * typingSpeed)
-            .SetEase(Ease.Linear)
-            .OnUpdate(UpdateText)
-            .OnComplete(OnTypingComplete);
+        _typewriterTween = DOTween.To(() => _currentCharIndex, x =>
+                                      {
+                                          if (x > _currentCharIndex)
+                                          {
+                                            Debug.Log("audioSource.Play();");
+                                            audioSource.PlayOneShot(audioSource.clip);
+                                              // audioSource.Play();
+                                          }
+                                          
+                                          _currentCharIndex = x;
+                                      }, fullText.Length,
+                                      fullText.Length * typingSpeed)
+                                  .SetEase(Ease.Linear)
+                                  .OnUpdate(UpdateText)
+                                  .OnComplete(OnTypingComplete);
     }
+
 
     void UpdateText()
     {
         textMeshPro.text = fullText.Substring(0, _currentCharIndex);
+        audioSource.Play();
     }
 
     void OnTypingComplete()
